3c1807f5cd9309ce5b5e08a76b74fbb8c79292fd
oiisadev <deweylabs.official@gmail.com>
2024-10-07 13:45:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');  // cors 패키지 불러오기
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());  // 모든 출처에서의 요청을 허용
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Render!' });
